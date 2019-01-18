242b8eeb7f8954b9635af1ddcb0c7795415d8091
EthenWilson <46796554+EthenWilson@users.noreply.github.com>
2019-01-18 14:05:09 -0700

    Update practice.py

--- practice.py
+++ practice.py
@@ -4,3 +4,5 @@ print("Welcome to the Practice Repository")
 print("Hello.")
 value = 5
 print("Value = " + str(value))
+print("Jy'Chaun has entered the chat")
+print("Jy'Chaun has left the chat (RIP)")
